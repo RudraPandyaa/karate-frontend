f3d7c2f30ebc5988de0820775108bdee14a0e511
Rudra <viralpandya079@gmail.com>
2026-07-17 19:18:37 +0530

    backend deplyment changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,33 +3,33 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-    modules: [
+  modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
-    '@nuxtjs/color-mode'
+    '@nuxtjs/color-mode',
   ],
 
-  // components: [
-  //   {
-  //     path: '~/components',
-  //     pathPrefix: false,
-  //   },
-  // ],
   app: {
     head: {
       title: 'WKF Manager | Tournament Dashboard',
-      meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+      ],
     },
   },
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE
-    }
-  }
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+    },
+  }, // <-- Missing comma was here
 
   css: ['~/assets/css/main.css'],
-    colorMode: {
-    classSuffix: ''
+
+  colorMode: {
+    classSuffix: '',
   },
 })
